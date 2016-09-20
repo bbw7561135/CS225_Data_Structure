81fd179e740b0fb8a4631d096dccbe5ef70bd9fc
chuericky <chuericky@users.noreply.github.com>
2016-09-20 17:05:59 -0500

    Add files via upload

---
+++ Daily_Exercise/q4/pet.h
@@ -0,0 +1,29 @@
+#ifndef _PET_H
+#define _PET_H
+
+#include <string>
+
+class Pet {
+ public:
+  // Declare your constructors here!
+    Pet();
+    Pet(std::string petname, int BY, std::string pettype, std::string oname);
+    ~Pet();
+  // Other member functions
+  void setName(std::string newName);
+  void setBY(int newBY);
+  void setType(std::string newType);
+  void setOwnerName(std::string newName);
+  std::string getName();
+  int getBY();
+  std::string getType();
+  std::string getOwnerName();
+
+ private:
+  std::string name;
+  int birth_year;
+  std::string type;
+  std::string owner_name;
+};
+
+#endif
